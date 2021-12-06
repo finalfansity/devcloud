7e6d329e1f00675eb6a25effe9ff79bd59fdf594
zhuhui <douma515253@gmail.com>
2021-12-06 17:24:09 +0800

    table

--- fronted/src/views/cmdb/host/index.vue
+++ fronted/src/views/cmdb/host/index.vue
@@ -1,21 +1,49 @@
 <template>
   <div class="main-container">
     <tips :tips="tips" />
-    Host 页面
+    <div>
+      <el-table :data="tableData" style="width: 100%" stripe="true">
+        <el-table-column prop="date" label="日期" width="180">
+        </el-table-column>
+        <el-table-column prop="name" label="姓名" width="180">
+        </el-table-column>
+        <el-table-column prop="address" label="地址"> </el-table-column>
+      </el-table>
+    </div>
   </div>
 </template>
 
 <script>
-import Tips from '@/components/Tips'
-const tips = [
-  '现在仅同步了阿里云主机资产'
-]
+import Tips from "@/components/Tips";
+const tips = ["现在仅支持同步主机资产"];
 export default {
   name: "Host",
-  components: {Tips},
+  components: { Tips },
   data() {
     return {
-      tips: tips
+      tips: tips,
+      tableData: [
+        {
+          date: "2016-05-02",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          date: "2016-05-04",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1517 弄",
+        },
+        {
+          date: "2016-05-01",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1519 弄",
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1516 弄",
+        },
+      ],
     };
   },
 };
